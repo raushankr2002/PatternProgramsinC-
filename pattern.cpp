8f26917e7d720cdc0961dad1b89af200ca582f62
raushankr2002 <90882981+raushankr2002@users.noreply.github.com>
2021-09-17 08:22:13 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*cout<<"Enter Number of Rows ::";
+    int rows;
+    cin>>rows;
+    cout<<"Enter Columns :: ";
+    int col;
+    cin>>col;
+    for(int i=1; i<=rows; i++){
+        for(int j=1; j<=col; j++){
+            cout<<"1";
+        }
+        cout<<endl;
+    }*/ 
+
+
+    /*
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"@";
+        }
+        cout<<endl;
+    }*/    
+
+
+
+    
+    /*
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    */
+
+
+   /*for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }*/
+
+    /*for(int i=1; i<=5; i++){
+        for(int j=5; j>=i; j--){
+            cout<<" #  ";
+            
+        }
+        cout<<endl;
+    }
+    */
+   /*for(char ch='A'; ch<='E'; ch++){
+       for(char chh='A'; chh<='D'; chh++){
+           cout<<chh;
+       }
+       cout<<endl;
+   }*/
+
+  /* for(int i=1; i<=5; i++){
+       for(int j=4; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=i; k++){
+          cout<<" $ ";
+       }
+       cout<<"\n";
+   }*/
+
+  /* for(int i=1; i<=5; i++){
+       for(int j=4; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=i; k++){
+           cout<<"#";
+       }
+       cout<<endl;
+   }*/
+  /* cout<<"Enter Number of rows ::";
+   int rows;
+   cin>>rows;
+   cout<<"Enter Number of columns ::";
+   int col;
+   cin>>col;
+   for(int i=1; i<=rows; i++){
+       for(int j=rows-1; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=rows; k++){
+           cout<<"$";
+       }
+       cout<<endl;
+   }*/
+  /* for(int i=1; i<=5; i++){
+       for(int j=4; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=i; k++){
+           cout<<"@";
+       }
+       cout<<endl;
+   }*/
+  /* for(int i=1; i<=5; i++){
+       for(int j=4; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=i; k++){
+           cout<<k;
+       }
+       cout<<endl;
+   }*/
+  /* for(int i=1; i<=5; i++){
+       for(int j=4; j>=i; j--){
+           cout<<" ";
+       }
+       for(int k=1; k<=i; k++){
+           cout<<i;
+       }
+       cout<<endl;
+   }*/
+   
+
+   return 0;
+}
